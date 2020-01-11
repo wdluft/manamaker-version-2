0531559b9781f357103de9d317ae2c18e368413e
Will Luft <william.d.luft@gmail.com>
2020-01-11 15:08:39 -0500

    Added and styled footer component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components'
+
+// Component imports
 import About from './about/About';
 import Header from './header/Header';
+import Footer from './footer/Footer';
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Switch>
-        <Route path='/about'>
-          <About />
-        </Route>
-        <Route path='/'>
-          <h1>Hello World</h1>
-        </Route>
-      </Switch>
+      <StyledApp>
+        <Header />
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/'>
+            <h1>Hello World</h1>
+          </Route>
+        </Switch>
+        <Footer />
+      </StyledApp>
     </Router>
   );
 }
 
 export default App;
+
+const StyledApp = styled.div`
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
---
+++ src/footer/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <h1>Made by <a href="https://willluft" target="_blank" rel="noopener noreferrer">Will Luft</a></h1>
+      <p><a href="https://twitter.com/iamwilldl" target="_blank" rel="noopener noreferrer">Twitter</a></p>
+    </StyledFooter>
+  )
+}
+
+export default Footer
+
+const StyledFooter = styled.footer`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 15px;
+
+  h1 {
+    font-size: 1.25rem;
+    border-bottom: 1px solid var(--light-color);
+  }
+
+  p {
+    font-size: 1rem;
+  }
+`;
