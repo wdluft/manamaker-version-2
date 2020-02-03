732c26b019b046ef4be1b0f20325fdf81822a1c3
Will Luft <william.d.luft@gmail.com>
2020-02-03 18:08:57 -0500

    Updated LandTypes component to use useContext hook

--- src/components/calculator/LandTypes.js
+++ src/components/calculator/LandTypes.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import LandType from './LandType';
 
 import { CalculatorContext } from '../../contexts/CalculatorContext';
 
-const LandTypes = () => (
-  <CalculatorContext.Consumer>
-    {context => {
-      const { manaColors } = context;
-      return (
-        <StyledLands>
-          {manaColors.map(color => (
-            <LandType key={color.id} manaColor={color} />
-          ))}
-        </StyledLands>
-      );
-    }}
-  </CalculatorContext.Consumer>
-);
+const LandTypes = () => {
+  const { manaColors } = useContext(CalculatorContext);
+  return (
+    <StyledLands>
+      {manaColors.map(color => (
+        <LandType key={color.id} manaColor={color} />
+      ))}
+    </StyledLands>
+  );
+};
 
 export default LandTypes;
 
